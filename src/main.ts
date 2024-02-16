c4f49205cfce5fcea370e368519de45b4969b81c
Andrey Briginets <andrewdymov@gmail.com>
2024-02-16 21:07:51 +0200

    fix: make winston default logger for nest

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
   await app.listen(3000);
 }
